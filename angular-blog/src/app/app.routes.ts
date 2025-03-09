e54dcb6a886fbdf08a6ee89a5cc7802572b9f41d
gabriel messias <gabrielmessias657@gmail.com>
2025-03-09 15:36:05 -0300

    content created

--- angular-blog/src/app/app.routes.ts
+++ angular-blog/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './pages/content/content.component';
 
 export const routes: Routes = [];
